8f1ed497cabedc3e94893d4a896f738c012b02f1
Jawahiir <jawahiirboss@gmail.com>
2024-05-09 15:11:14 +0600

    Add project files.

---
+++ BlackLagoon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlackLagoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
