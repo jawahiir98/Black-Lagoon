fb48b977f465918865242f344ec101d49423cb3f
Jawahiir <jawahiirboss@gmail.com>
2024-05-10 11:50:25 +0600

    2. Villa Model: Controller

---
+++ BlackLagoon/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using BlackLagoon.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public VillaController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            var Villas = db.Villas.ToList();
+            return View(Villas);
+        }
+    }
+}
