e038050905ff5b1a677488b42bb21e0404214b95
Jawahiir <jawahiirboss@gmail.com>
2024-05-11 09:22:34 +0600

    5. Villa Number: List All Villa Numbers

---
+++ BlackLagoon/Controllers/VillaNumberController.cs
@@ -0,0 +1,19 @@
+﻿using BlackLagoon.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackLagoon.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public VillaNumberController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            var villaNumbers = db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+    }
+}
