c4c58c9fa4046d12131e3ee4ece5bb2212dd05c6
Jawahiir <jawahiirboss@gmail.com>
2024-05-11 09:13:17 +0600

    5. Villa Number: Add & Seed Villa Number Table to DB

---
+++ BlackLagoon.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackLagoon.Domain.Entities
+{
+    public class VillaNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Villa_Number { get; set; }
+        [ForeignKey("Villa")]
+        public int VillaId { get; set; }
+        public Villa? Villa { get; set; }
+        public string? SpecialDetails { get; set; }
+    }
+}
--- BlackLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ BlackLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace BlackLagoon.Infrastructure.Data
             
         }
         public DbSet<Villa> Villas { get; set; }
+        public DbSet<VillaNumber> VillaNumbers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //base.OnModelCreating(modelBuilder);
@@ -50,6 +51,53 @@ namespace BlackLagoon.Infrastructure.Data
                     Sqft = 750,
                 }
             );
+            modelBuilder.Entity<VillaNumber>().HasData(
+                new VillaNumber
+                {
+                    Villa_Number = 101,
+                    VillaId = 1,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 102,
+                    VillaId = 1,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 103,
+                    VillaId = 1,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 104,
+                    VillaId = 1,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 201,
+                    VillaId = 2,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 202,
+                    VillaId = 2,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 203,
+                    VillaId = 2,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 301,
+                    VillaId = 3,
+                },
+                new VillaNumber
+                {
+                    Villa_Number = 302,
+                    VillaId = 3,
+                }
+                );
         }
     }
 }
