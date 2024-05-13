47bab510a8cdc6ab6be6ca449adc0a6dde96c120
Jawahiir <jawahiirboss@gmail.com>
2024-05-13 23:28:41 +0600

    5. Villa Number: Delete Method, UI & POST Action

--- BlackLagoon/Controllers/VillaNumberController.cs
+++ BlackLagoon/Controllers/VillaNumberController.cs
@@ -90,5 +90,37 @@ namespace BlackLagoon.Web.Controllers
 
             return View(obj);
         }
+        public IActionResult Delete(int villaNumberId)
+        {
+            VillaNumberVM villaNumberVM = new()
+            {
+                VillaList = db.Villas.ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                VillaNumber = db.VillaNumbers.FirstOrDefault(u => u.Villa_Number == villaNumberId)
+            };
+            if (villaNumberVM.VillaNumber == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villaNumberVM);
+        }
+        [HttpPost]
+        public IActionResult Delete(VillaNumberVM villaNumberVM)
+        {
+            VillaNumber? objFromDb = db.VillaNumbers
+                .FirstOrDefault(u => u.Villa_Number == villaNumberVM.VillaNumber.Villa_Number);
+            if (objFromDb is not null)
+            {
+                db.VillaNumbers.Remove(objFromDb);
+                db.SaveChanges();
+                TempData["success"] = "The villa number has been deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Villa number could not be deleted.";
+            return View();
+        }
     }
 }
