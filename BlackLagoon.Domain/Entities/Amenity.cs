7f868cccdc8a5c3074bd7b43070cf6409c6201f4
Jawahiir <jawahiirboss@gmail.com>
2024-05-15 23:41:57 +0600

    8. Amenity: Amenity Table & Seed Data

---
+++ BlackLagoon.Domain/Entities/Amenity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackLagoon.Domain.Entities
+{
+    public class Amenity
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        [ForeignKey("Villa")]
+        public int VillaId { get; set; }
+        public Villa Villa { get; set; }
+    }
+}
--- BlackLagoon.Infrastructure/Migrations/20240515174029_addAmenityTable.Designer.cs
+++ BlackLagoon.Infrastructure/Migrations/20240515174029_addAmenityTable.Designer.cs
@@ -4,6 +4,7 @@ using BlackLagoon.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlackLagoon.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240515174029_addAmenityTable")]
+    partial class addAmenityTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
